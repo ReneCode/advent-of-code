e10cf58c3730ed93e06a48c9e77813e5e249ae6c
Reinhard Langner <ReinhardLangner@gmx.de>
2023-01-19 21:51:57 +0100

    day01 - part1

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn read_lines(filename: &str) -> io::Lines<BufReader<File>> {
+    let file = File::open(filename).unwrap();
+    return io::BufReader::new(file).lines();
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, day01");
+
+    let mut total_fuel_value: f32 = 0.0;
+    let lines = read_lines("./01.data");
+    for line in lines {
+        match line.unwrap().parse::<f32>() {
+            Ok(val) => {
+                let fuel_value = ((val / 3.0) - 0.5).round() - 2.0;
+                total_fuel_value = total_fuel_value + fuel_value;
+                // println!("{} / {}", val, result);
+            }
+            Err(_) => {}
+        }
+    }
+    println!("total fuel value {}", total_fuel_value);
 }
