3130ba2116e12f96af26adf8c826c73644aaf009
Reinhard Langner <ReinhardLangner@gmx.de>
2023-01-20 16:19:34 +0100

    day01 - part 2

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -4,20 +4,72 @@ mod util;
 
 fn main() {
     println!("Hello, day01");
+    part_1();
+    part_2();
+}
 
-    let mut total_fuel_value: f32 = 0.0;
+fn part_1() {
+    let mut total_fuel_value: i32 = 0;
 
     if let Some(lines) = util::get_lines("./01.data") {
         for line in lines {
-            match line.parse::<f32>() {
+            match line.parse::<i32>() {
                 Ok(mass) => {
-                    let fuel_value = (mass / 3.0).floor() - 2.0;
+                    let fuel_value = calc_required(mass);
                     total_fuel_value = total_fuel_value + fuel_value;
-                    println!("{} / {}", mass, fuel_value);
+                    // println!("{} / {}", mass, fuel_value);
                 }
                 Err(_) => {}
             }
         }
     }
-    println!("total fuel value {}", total_fuel_value);
+    println!("part-1 total fuel value {}", total_fuel_value);
+}
+
+fn parse_f32_or_0(str: &str) -> f32 {
+    match str.parse::<f32>() {
+        Ok(m) => m,
+        Err(_) => 0.0,
+    }
+}
+
+fn parse_i32_or_0(str: &str) -> i32 {
+    match str.parse::<i32>() {
+        Ok(m) => m,
+        Err(_) => 0,
+    }
+}
+
+fn part_2() {
+    if let Some(lines) = util::get_lines("./01.data") {
+        let total = lines
+            .iter()
+            .map(|line| parse_i32_or_0(line))
+            .map(|mass| calc_required_part_2(mass))
+            .reduce(|a, b| a + b);
+        if let Some(result) = total {
+            println!("part-2 total {}", result);
+        };
+    }
+}
+
+fn calc_required(mass: i32) -> i32 {
+    let fuel_value = (mass as f32 / 3.0).floor() - 2.0;
+    fuel_value as i32
+}
+
+fn calc_required_part_2(mass: i32) -> i32 {
+    let mut result = 0;
+    let mut val = mass;
+    loop {
+        let req = calc_required(val);
+        if req > 0 {
+            result += req;
+            val = req;
+        } else {
+            break;
+        }
+    }
+    // println!("requ {} / {}", mass, result);
+    result
 }
