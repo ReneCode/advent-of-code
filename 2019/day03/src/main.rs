0527d7efeeb7ce252c039ce6440fc93a50a07c8a
Reinhard Langner <ReinhardLangner@gmx.de>
2023-01-27 18:29:34 +0100

    day03 - part1

--- 2019/day03/src/main.rs
+++ 2019/day03/src/main.rs
@@ -1,13 +1,90 @@
 extern crate util;
 
-struct Data {
-    first_points: Vec<Point>,
-    second_points: Vec<Point>,
+struct BoundingBox {
+    x_min: i32,
+    x_max: i32,
+    y_min: i32,
+    y_max: i32,
+}
+
+impl BoundingBox {
+    fn new(x: i32, y: i32) -> BoundingBox {
+        BoundingBox {
+            x_min: x,
+            x_max: x,
+            y_min: y,
+            y_max: y,
+        }
+    }
+    fn add(&mut self, x: i32, y: i32) {
+        self.x_min = self.x_min.min(x);
+        self.x_max = self.x_max.max(x);
+        self.y_min = self.y_min.min(y);
+        self.y_max = self.y_max.max(y);
+    }
 }
 
 struct Point {
     x: i32,
     y: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+    fn clone(&self) -> Self {
+        Point::new(self.x, self.y)
+    }
+}
+
+struct Line {
+    p1: Point,
+    p2: Point,
+}
+
+fn between(a: i32, r1: i32, r2: i32) -> bool {
+    (r1 < a && a < r2) || (r2 < a && a < r1)
+}
+
+impl Line {
+    fn new(p1: &Point, p2: &Point) -> Line {
+        Line {
+            p1: p1.clone(),
+            p2: p2.clone(),
+        }
+    }
+
+    fn is_horizontal(&self) -> bool {
+        self.p1.y == self.p2.y
+    }
+
+    fn is_vertical(&self) -> bool {
+        self.p1.x == self.p2.x
+    }
+
+    fn intersect(&self, other: &Line) -> Option<Point> {
+        if self.is_horizontal()
+            && other.is_vertical()
+            && between(other.p1.x, self.p1.x, self.p2.x)
+            && between(self.p1.y, other.p1.y, other.p2.y)
+        {
+            return Some(Point::new(other.p1.x, self.p1.y));
+        }
+        if self.is_vertical()
+            && other.is_horizontal()
+            && between(self.p1.x, other.p1.x, other.p2.x)
+            && between(other.p1.y, self.p1.y, self.p2.y)
+        {
+            return Some(Point::new(self.p1.x, other.p1.y));
+        }
+        None
+    }
+}
+
+struct Board {
+    first_lines: Vec<Line>,
+    second_lines: Vec<Line>,
 }
 
 fn convert_to_points(path: &str) -> Vec<Point> {
@@ -42,12 +119,31 @@ fn convert_to_points(path: &str) -> Vec<Point> {
     points
 }
 
-fn get_data(filename: &str) -> Option<Data> {
+fn get_data(filename: &str) -> Option<Board> {
     if let Some(input) = util::io::get_lines(filename) {
-        // let points = convert_to_points(&input[0]);
-        let data = Data {
-            first_points: convert_to_points(&input[0]),
-            second_points: convert_to_points(&input[1]),
+        let first_points = convert_to_points(&input[0]);
+        let second_points = convert_to_points(&input[1]);
+        let mut bounding_box = BoundingBox::new(0, 0);
+        for point in first_points.iter() {
+            bounding_box.add(point.x, point.y);
+        }
+        for point in second_points.iter() {
+            bounding_box.add(point.x, point.y);
+        }
+
+        let first_lines: Vec<Line> = first_points
+            .windows(2)
+            .map(|window| Line::new(&window[0], &window[1]))
+            .collect();
+
+        let second_lines: Vec<Line> = second_points
+            .windows(2)
+            .map(|points| Line::new(&points[0], &points[1]))
+            .collect();
+
+        let data = Board {
+            first_lines,
+            second_lines,
         };
         Some(data)
     } else {
@@ -57,9 +153,26 @@ fn get_data(filename: &str) -> Option<Data> {
 
 fn main() {
     println!("Hello, day03!");
-    if let Some(data) = get_data("./03-example.data") {
-        let a = 42;
-        println!("{a}")
-        // println!("{:?} / {:?}", data.first_points, data.first_points);
+    if let Some(board) = get_data("./03.data") {
+        part_1(&board);
     }
+}
+
+fn part_1(board: &Board) {
+    let mut intersections = Vec::new();
+    for first_line in board.first_lines.iter() {
+        for second_line in board.second_lines.iter() {
+            if let Some(intersect) = first_line.intersect(&second_line) {
+                intersections.push(intersect)
+            }
+        }
+    }
+
+    let mut distances: Vec<i32> = intersections
+        .iter()
+        .map(|p| p.x.abs() + p.y.abs())
+        .collect();
+    distances.sort();
+
+    println!("part1 min distance {}", distances[0]);
 }
