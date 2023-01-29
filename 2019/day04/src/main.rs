5d076dd3ad6475689378d5b6087e0ad91999cb40
Reinhard Langner <ReinhardLangner@gmx.de>
2023-01-29 15:50:58 +0100

    day04 - part2

--- 2019/day04/src/main.rs
+++ 2019/day04/src/main.rs
@@ -1,4 +1,4 @@
-use core::num;
+use std::collections::HashMap;
 
 fn main() {
     println!("Hello, day04!");
@@ -10,29 +10,32 @@ fn main() {
         .map(|s| s.parse::<i32>().unwrap() as i32)
         .collect();
 
-    part_1(from_to);
+    let result_1 = count_valid_numbers(&from_to, check_valid_password_part_1);
+    println!("part-1 total: {}", result_1);
+
+    let result_2 = count_valid_numbers(&from_to, check_valid_password_part_2);
+    println!("part-2 total: {}", result_2);
 }
 
-fn part_1(from_to: Vec<i32>) {
+fn count_valid_numbers(from_to: &Vec<i32>, check_fn: fn(i32) -> bool) -> i32 {
     let mut nr = from_to[0];
     let mut total = 0;
     while nr <= from_to[1] {
-        if check_valid_password(nr) {
+        if check_fn(nr) {
             total += 1;
         }
         nr += 1;
     }
-    println!("part-1 total: {}", total);
+    total
 }
 
-fn check_valid_password(nr: i32) -> bool {
+fn check_valid_password_part_1(nr: i32) -> bool {
     let numbers: Vec<i32> = nr
         .to_string()
         .chars()
         .map(|c| c as i32 - '0' as i32)
         .collect();
 
-    let ok = true;
     let mut prev_nr = -1;
     let mut duplicate = false;
     for nr in numbers {
@@ -49,4 +52,41 @@ fn check_valid_password(nr: i32) -> bool {
     }
 
     true
+}
+
+#[test]
+fn test_check_part2() {
+    assert_eq!(true, check_valid_password_part_2(112233));
+    assert_eq!(false, check_valid_password_part_2(123444));
+    assert_eq!(true, check_valid_password_part_2(111122));
+}
+
+fn check_valid_password_part_2(nr: i32) -> bool {
+    let numbers: Vec<i32> = nr
+        .to_string()
+        .chars()
+        .map(|c| c as i32 - '0' as i32)
+        .collect();
+
+    let mut prev_nr = -1;
+    let mut counts = HashMap::new();
+    for nr in numbers {
+        if nr < prev_nr {
+            return false;
+        }
+        if let Some(count) = counts.get_mut(&nr) {
+            *count += 1;
+        } else {
+            counts.insert(nr, 1);
+        }
+        prev_nr = nr;
+    }
+    let mut ok = false;
+    for (k, v) in counts {
+        if v == 2 {
+            ok = true;
+        }
+    }
+
+    ok
 }
