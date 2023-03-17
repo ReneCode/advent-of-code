ade3c217240b0915ff2bd471971b560d95dbf021
Reinhard Langner <ReinhardLangner@gmx.de>
2023-03-17 19:53:50 +0100

    day14 - part1  current version

---
+++ 2019/day14/readme.md
@@ -0,0 +1,41 @@
+```
+
+157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT
+
+---
+
+1 FUEL
+  44 XJWVT  => create:22*2
+    154 DCFZ   => create:26*6 /  stock:2
+      4290 ORE
+    154 PSHF   => create22*7
+      3938 ORE
+
+  5 KHKGT => create 1*8 /  stock:3
+    3 DCFZ  =>  create 1*6 / stock 2+3 = 5
+      165 ORE
+    7 NZVS  => create 2*5 / stock:3
+      314 ORE
+    5 HKGWZ => create 1*5
+      177 ORE
+    10 PSHF => create 2*7 / stock:4
+      358 ORE
+
+  1 QDVJ => create 1*9 / stock:8
+    12 HKGWZ => create 3*5 / stock:3
+      531 ORE
+    1 GPVTF => create 1*2 / stock:1
+      165 ORE
+    8 PSHF => create 1*7+4 / stock:3
+      179 ORE
+
+
+```
--- 2019/day14/src/main.rs
+++ 2019/day14/src/main.rs
@@ -101,13 +101,24 @@ fn calc_count_recipe_and_stock(
 ) -> (i32, Option<i32>) {
     let mut count_recipe = count_needed / count_target;
     let mut opt_stock = None;
-    let rest = count_needed % count_target;
-    if rest > 0 {
-        if rest <= stock {
-            opt_stock = Some(stock - rest);
+    if count_needed >= count_target {
+        let rest = count_needed % count_target;
+        if rest > 0 {
+            if rest <= stock {
+                opt_stock = Some(stock - rest);
+            } else {
+                count_recipe += 1;
+                opt_stock = Some(stock + count_target - rest)
+            }
+        }
+    } else {
+        if stock >= count_needed {
+            count_recipe = 0;
+            opt_stock = Some(stock - count_needed)
         } else {
-            count_recipe += 1;
-            opt_stock = Some(stock + count_target - rest)
+            count_recipe = 1;
+            let rest = count_target - count_needed;
+            opt_stock = Some(stock + rest)
         }
     }
     (count_recipe, opt_stock)
@@ -137,34 +148,57 @@ fn test_4() {
     assert_eq!(count_recipe, 1);
     assert_eq!(opt_stock, Some(2));
 }
+#[test]
+fn test_5() {
+    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(7, 4, 1);
+    assert_eq!(count_recipe, 1);
+    assert_eq!(opt_stock, Some(4));
+}
+#[test]
+fn test_5a() {
+    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(11, 7, 9);
+    assert_eq!(count_recipe, 0);
+    assert_eq!(opt_stock, Some(2));
+}
+#[test]
+fn test_6() {
+    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(11, 7, 5);
+    assert_eq!(count_recipe, 1);
+    assert_eq!(opt_stock, Some(9));
+}
 
 fn get_data(filename: &str) -> Option<HashMap<String, Recipe>> {
     if let Some(lines) = io::get_lines(filename) {
-        let mut result: HashMap<String, Recipe> = HashMap::new();
-        for line in lines {
-            let token: Vec<&str> = line.split(" => ").collect();
-
-            let sources: Vec<CountName> = token[0]
-                .split(',')
-                .map(|l| l.trim())
-                .map(CountName::from)
-                .collect();
-
-            let target = CountName::from(token[1]);
-            let name = target.name.clone();
-            let recipe = Recipe { sources, target };
-            result.insert(name, recipe);
-        }
-
-        Some(result)
+        let l: Vec<&str> = lines.iter().map(|s| s.as_str()).collect();
+        parse_lines(l)
     } else {
         None
     }
 }
 
+fn parse_lines(lines: Vec<&str>) -> Option<HashMap<String, Recipe>> {
+    let mut result: HashMap<String, Recipe> = HashMap::new();
+    for line in lines {
+        let token: Vec<&str> = line.split(" => ").collect();
+
+        let sources: Vec<CountName> = token[0]
+            .split(',')
+            .map(|l| l.trim())
+            .map(CountName::from)
+            .collect();
+
+        let target = CountName::from(token[1]);
+        let name = target.name.clone();
+        let recipe = Recipe { sources, target };
+        result.insert(name, recipe);
+    }
+
+    Some(result)
+}
+
 fn main() {
     println!("Hello, day14!");
-    if let Some(recipes) = get_data("./14-example.data") {
+    if let Some(recipes) = get_data("./14.data") {
         part_1(&recipes);
     }
 }
@@ -173,4 +207,95 @@ fn part_1(recipes: &HashMap<String, Recipe>) {
     let mut solver = Solver::new(recipes);
     let result = solver.solve();
     println!("part1 {result}");
+}
+
+#[test]
+fn test_p1_1() {
+    let input = r#"10 ORE => 10 A
+    1 ORE => 1 B
+    7 A, 1 B => 1 C
+    7 A, 1 C => 1 D
+    7 A, 1 D => 1 E
+    7 A, 1 E => 1 FUEL"#;
+    let lines: Vec<&str> = input.split('\n').collect();
+    let recipes = parse_lines(lines).unwrap();
+    let mut solver = Solver::new(&recipes);
+    assert_eq!(solver.solve(), 31);
+}
+
+#[test]
+fn test_p1_2() {
+    let input = r#"9 ORE => 2 A
+    8 ORE => 3 B
+    7 ORE => 5 C
+    3 A, 4 B => 1 AB
+    5 B, 7 C => 1 BC
+    4 C, 1 A => 1 CA
+    2 AB, 3 BC, 4 CA => 1 FUEL"#;
+    let lines: Vec<&str> = input.split('\n').collect();
+    let recipes = parse_lines(lines).unwrap();
+    let mut solver = Solver::new(&recipes);
+    assert_eq!(solver.solve(), 165);
+}
+
+#[test]
+fn test_p1_3() {
+    let input = r#"157 ORE => 5 NZVS
+    165 ORE => 6 DCFZ
+    44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+    12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+    179 ORE => 7 PSHF
+    177 ORE => 5 HKGWZ
+    7 DCFZ, 7 PSHF => 2 XJWVT
+    165 ORE => 2 GPVTF
+    3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"#;
+    let lines: Vec<&str> = input.split('\n').collect();
+    let recipes = parse_lines(lines).unwrap();
+    let mut solver = Solver::new(&recipes);
+    assert_eq!(solver.solve(), 13312);
+}
+
+#[test]
+fn test_p1_4() {
+    let input = r#"2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+    17 NVRVD, 3 JNWZP => 8 VPVL
+    53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+    22 VJHF, 37 MNCFX => 5 FWMGM
+    139 ORE => 4 NVRVD
+    144 ORE => 7 JNWZP
+    5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+    5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+    145 ORE => 6 MNCFX
+    1 NVRVD => 8 CXFTF
+    1 VJHF, 6 MNCFX => 4 RFSQX
+    176 ORE => 6 VJHF"#;
+    let lines: Vec<&str> = input.split('\n').collect();
+    let recipes = parse_lines(lines).unwrap();
+    let mut solver = Solver::new(&recipes);
+    assert_eq!(solver.solve(), 180697);
+}
+
+#[test]
+fn test_p1_5() {
+    let input = r#"171 ORE => 8 CNZTR
+    7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+    114 ORE => 4 BHXH
+    14 VRPVC => 6 BMBT
+    6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+    6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+    15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+    13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+    5 BMBT => 4 WPTQ
+    189 ORE => 9 KTJDG
+    1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+    12 VRPVC, 27 CNZTR => 2 XDBXC
+    15 KTJDG, 12 BHXH => 5 XCVML
+    3 BHXH, 2 VRPVC => 7 MZWV
+    121 ORE => 7 VRPVC
+    7 XCVML => 6 RJRHP
+    5 BHXH, 4 VRPVC => 5 LTCX"#;
+    let lines: Vec<&str> = input.split('\n').collect();
+    let recipes = parse_lines(lines).unwrap();
+    let mut solver = Solver::new(&recipes);
+    assert_eq!(solver.solve(), 2210736);
 }
