670676907f57086213e7ca65a738f2fae95f2729
Reinhard Langner <ReinhardLangner@gmx.de>
2023-03-19 16:12:24 +0100

    day14 - part1

--- 2019/day14/src/main.rs
+++ 2019/day14/src/main.rs
@@ -1,9 +1,6 @@
 use util::io;
 
-use std::{
-    collections::{HashMap, VecDeque},
-    convert::From,
-};
+use std::{collections::HashMap, convert::From};
 
 const NAME_ORE: &str = "ORE";
 
@@ -32,30 +29,9 @@ struct Recipe {
     target: CountName,
 }
 
-struct Solver<'a> {
-    recipes: &'a HashMap<String, Recipe>,
-    stock: HashMap<String, i32>,
-    count_oer: i32,
-}
-
-impl<'a> Solver<'_> {
-    fn new(recipes: &HashMap<String, Recipe>) -> Solver {
-        Solver {
-            recipes,
-            stock: HashMap::new(),
-            count_oer: 0,
-        }
-    }
-
-    fn solve(&self) -> i32 {
-        calc_ore(self.recipes)
-        // calc_needed_ore(self.recipes, "FUEL")
-    }
-}
-
 fn calc_ore(recipes: &HashMap<String, Recipe>) -> i32 {
-    let mut ingrediences: VecDeque<CountName> = VecDeque::new();
-    ingrediences.push_back(CountName::new(String::from("FUEL"), 1));
+    let mut ingrediences: Vec<CountName> = Vec::new();
+    ingrediences.push(CountName::new(String::from("FUEL"), 1));
 
     let mut reserves: HashMap<String, i32> = HashMap::new();
     for name in recipes.keys() {
@@ -64,7 +40,7 @@ fn calc_ore(recipes: &HashMap<String, Recipe>) -> i32 {
 
     let mut ore_count = 0;
     while !ingrediences.is_empty() {
-        let wanted_ingredience = ingrediences.pop_front().unwrap();
+        let wanted_ingredience = ingrediences.pop().unwrap();
         println!(
             "want: {} {}",
             wanted_ingredience.count, wanted_ingredience.name
@@ -97,7 +73,7 @@ fn calc_ore(recipes: &HashMap<String, Recipe>) -> i32 {
                     {
                         future_ingredience.count += build_count * sub_ingredience.count;
                     } else {
-                        ingrediences.push_back(CountName::new(
+                        ingrediences.push(CountName::new(
                             sub_ingredience.name.clone(),
                             build_count * sub_ingredience.count,
                         ));
@@ -116,133 +92,6 @@ fn have_to_build(need_total: i32, package_count: i32) -> i32 {
         build += 1;
     }
     build
-}
-
-// copy from https://0xdf.gitlab.io/adventofcode2019/14
-fn calc_needed_ore(recipes: &HashMap<String, Recipe>, build: &str) -> i32 {
-    let mut stock: HashMap<String, i32> = HashMap::new();
-    let mut need: HashMap<String, i32> = HashMap::new();
-    need.insert(String::from(build), 1);
-    let mut ore_count = 0;
-
-    while !need.is_empty() {
-        let ele = need.keys().next().unwrap().clone();
-
-        let needed_count = *need.get(&ele).unwrap();
-        let stock_count = if let Some(stock) = stock.get(&ele) {
-            *stock
-        } else {
-            0
-        };
-
-        println!("build: {needed_count} {ele}");
-
-        if needed_count <= stock_count {
-            // enough in stock
-            need.remove(&ele);
-            stock.insert(ele, stock_count - needed_count);
-            continue;
-        }
-
-        let needed_count = needed_count - stock_count;
-
-        stock.remove(&ele);
-        need.remove(&ele);
-        if let Some(ele_recipe) = recipes.get(&ele) {
-            let recipe_count = ele_recipe.target.count;
-            let mut build_count = needed_count / recipe_count;
-            if needed_count % recipe_count > 0 {
-                build_count += 1;
-            }
-            stock.insert(ele.clone(), build_count * recipe_count - needed_count);
-            for target in ele_recipe.sources.iter() {
-                if target.name == NAME_ORE {
-                    ore_count += target.count * build_count;
-                } else {
-                    if let Some(waiting) = need.get_mut(&target.name) {
-                        *waiting += target.count * build_count;
-                    } else {
-                        need.insert(target.name.clone(), target.count * build_count);
-                    }
-                }
-            }
-        }
-    }
-
-    ore_count
-}
-
-fn calc_count_recipe_and_stock(
-    count_target: i32,
-    count_needed: i32,
-    stock: i32,
-) -> (i32, Option<i32>) {
-    let mut count_recipe = count_needed / count_target;
-    let mut opt_stock = None;
-    if count_needed >= count_target {
-        let rest = count_needed % count_target;
-        if rest > 0 {
-            if rest <= stock {
-                opt_stock = Some(stock - rest);
-            } else {
-                count_recipe += 1;
-                opt_stock = Some(stock + count_target - rest)
-            }
-        }
-    } else {
-        if stock >= count_needed {
-            count_recipe = 0;
-            opt_stock = Some(stock - count_needed)
-        } else {
-            count_recipe = 1;
-            let rest = count_target - count_needed;
-            opt_stock = Some(stock + rest)
-        }
-    }
-    (count_recipe, opt_stock)
-}
-
-#[test]
-fn test_1() {
-    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(2, 22, 0);
-    assert_eq!(count_recipe, 11);
-    assert_eq!(opt_stock, None);
-}
-#[test]
-fn test_2() {
-    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(5, 7, 0);
-    assert_eq!(count_recipe, 2);
-    assert_eq!(opt_stock, Some(3));
-}
-#[test]
-fn test_3() {
-    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(5, 7, 1);
-    assert_eq!(count_recipe, 2);
-    assert_eq!(opt_stock, Some(4));
-}
-#[test]
-fn test_4() {
-    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(5, 7, 4);
-    assert_eq!(count_recipe, 1);
-    assert_eq!(opt_stock, Some(2));
-}
-#[test]
-fn test_5() {
-    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(7, 4, 1);
-    assert_eq!(count_recipe, 1);
-    assert_eq!(opt_stock, Some(4));
-}
-#[test]
-fn test_5a() {
-    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(11, 7, 9);
-    assert_eq!(count_recipe, 0);
-    assert_eq!(opt_stock, Some(2));
-}
-#[test]
-fn test_6() {
-    let (count_recipe, opt_stock) = calc_count_recipe_and_stock(11, 7, 5);
-    assert_eq!(count_recipe, 1);
-    assert_eq!(opt_stock, Some(9));
 }
 
 fn get_data(filename: &str) -> Option<HashMap<String, Recipe>> {
@@ -282,8 +131,7 @@ fn main() {
 }
 
 fn part_1(recipes: &HashMap<String, Recipe>) {
-    let mut solver = Solver::new(recipes);
-    let result = solver.solve();
+    let result = calc_ore(recipes);
     println!("part1 {result}");
 }
 
@@ -297,8 +145,7 @@ fn test_p1_1() {
     7 A, 1 E => 1 FUEL"#;
     let lines: Vec<&str> = input.split('\n').collect();
     let recipes = parse_lines(lines).unwrap();
-    let mut solver = Solver::new(&recipes);
-    assert_eq!(solver.solve(), 31);
+    assert_eq!(calc_ore(&recipes), 31);
 }
 
 #[test]
@@ -312,8 +159,7 @@ fn test_p1_2() {
     2 AB, 3 BC, 4 CA => 1 FUEL"#;
     let lines: Vec<&str> = input.split('\n').collect();
     let recipes = parse_lines(lines).unwrap();
-    let mut solver = Solver::new(&recipes);
-    assert_eq!(solver.solve(), 165);
+    assert_eq!(calc_ore(&recipes), 165);
 }
 
 #[test]
@@ -329,8 +175,7 @@ fn test_p1_3() {
     3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"#;
     let lines: Vec<&str> = input.split('\n').collect();
     let recipes = parse_lines(lines).unwrap();
-    let mut solver = Solver::new(&recipes);
-    assert_eq!(solver.solve(), 13312);
+    assert_eq!(calc_ore(&recipes), 13312);
 }
 
 #[test]
@@ -349,8 +194,7 @@ fn test_p1_4() {
     176 ORE => 6 VJHF"#;
     let lines: Vec<&str> = input.split('\n').collect();
     let recipes = parse_lines(lines).unwrap();
-    let mut solver = Solver::new(&recipes);
-    assert_eq!(solver.solve(), 180697);
+    assert_eq!(calc_ore(&recipes), 180697);
 }
 
 #[test]
@@ -374,6 +218,5 @@ fn test_p1_5() {
     5 BHXH, 4 VRPVC => 5 LTCX"#;
     let lines: Vec<&str> = input.split('\n').collect();
     let recipes = parse_lines(lines).unwrap();
-    let mut solver = Solver::new(&recipes);
-    assert_eq!(solver.solve(), 2210736);
+    assert_eq!(calc_ore(&recipes), 2210736);
 }
