d6d55995a1ee7fd711fb8a38be92fc9ef2597638
Reinhard Langner <ReinhardLangner@gmx.de>
2023-12-01 11:48:49 +0100

    day01 / 2023

--- 2020/src/day04/mod.rs
+++ 2020/src/day04/mod.rs
@@ -12,36 +12,58 @@ pub fn day04() {
     let all_lines = lines.iter().join("\n");
     let passports: Vec<&str> = all_lines.split("\n\n").collect();
 
-    let mut count_valid = 0;
-    for passport in passports {
-        let lines = passport.split("\n").collect_vec();
-        let keys = get_keys(&lines);
-        if all_required_keys_there(&keys) {
-            count_valid += 1;
-        }
-        // println!("passport lines: {:?}", lines);
-    }
+    for part in ['A', 'B'] {
+        let mut count_valid = 0;
+        for passport in passports.iter() {
+            let lines = passport.split("\n").collect_vec();
+            let key_value_pairs = get_key_value_pairs(&lines);
 
-    println!("result A: {count_valid}");
+            let ok = match part {
+                'A' => all_required_keys_there(&key_value_pairs),
+                'B' => all_required_keys_there(&key_value_pairs) && all_values_ok(&key_value_pairs),
+                _ => false,
+            };
+            if ok {
+                count_valid += 1
+            }
+            // println!("passport lines: {:?}", lines);
+        }
+
+        println!("result {part}: {count_valid}");
+    }
 }
 
-fn all_required_keys_there(keys: &[&str]) -> bool {
+fn all_required_keys_there(key_value_pairs: &[(&str, &str)]) -> bool {
     for key in REQUIRED_KEYS {
-        if !keys.contains(&key) {
+        if let None = key_value_pairs.iter().find(|(k, v)| *k == key) {
             return false;
         }
     }
     true
 }
 
-fn get_keys<'a>(lines: &'a Vec<&'a str>) -> Vec<&'a str> {
-    let mut result: Vec<&str> = Vec::new();
+fn all_values_ok(key_value_pairs: &[(&str, &str)]) -> bool {
+    for (key, value) in key_value_pairs {
+        let ok = match (*key) {
+            "byr" => true,
+            _ => true,
+        };
+        if !ok {
+            return false;
+        }
+    }
+    true
+}
+
+fn get_key_value_pairs<'a>(lines: &'a Vec<&'a str>) -> Vec<(&'a str, &str)> {
+    let mut result: Vec<(&str, &str)> = Vec::new();
     for line in lines {
         let kv_pairs = line.split(' ').map(|l| l.trim()).collect_vec();
         for pair in kv_pairs {
             let token = pair.split(':').map(|t| t.trim()).collect_vec();
             let key = token.get(0).unwrap();
-            result.push(*key);
+            let value = token.get(1).unwrap();
+            result.push((*key, *value));
         }
     }
 
--- 2023/src/day01/mod.rs
+++ 2023/src/day01/mod.rs
@@ -1,5 +1,89 @@
 // day01
 
+use core::slice;
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
+use crate::util::io;
+
 pub fn day01() {
     println!("hello day 01");
+
+    let lines = io::read_lines("./01.data").unwrap();
+
+    let sum: i32 = lines.iter().map(|line| get_nr_from_digit(line)).sum();
+    println!("Result A: {sum}");
+
+    let sum: i32 = lines
+        .iter()
+        .map(|line| get_nr_from_digit_or_name(line))
+        .sum();
+    println!("Result B: {sum}");
+}
+
+fn get_nr_from_digit(line: &str) -> i32 {
+    let first = line.chars().find(|c| c.is_digit(10)).unwrap();
+    let last = line.chars().rev().find(|c| c.is_digit(10)).unwrap();
+
+    let mut complete: String = String::new();
+    complete.push(first);
+    complete.push(last);
+    let nr = complete.parse().unwrap();
+    nr
+}
+
+fn get_nr_from_digit_or_name(line: &str) -> i32 {
+    let mut str_to_val: HashMap<String, i32> = HashMap::new();
+    str_to_val.insert("0".to_string(), 0);
+    str_to_val.insert("1".to_string(), 1);
+    str_to_val.insert("2".to_string(), 2);
+    str_to_val.insert("3".to_string(), 3);
+    str_to_val.insert("4".to_string(), 4);
+    str_to_val.insert("5".to_string(), 5);
+    str_to_val.insert("6".to_string(), 6);
+    str_to_val.insert("7".to_string(), 7);
+    str_to_val.insert("8".to_string(), 8);
+    str_to_val.insert("9".to_string(), 9);
+    str_to_val.insert("one".to_string(), 1);
+    str_to_val.insert("two".to_string(), 2);
+    str_to_val.insert("three".to_string(), 3);
+    str_to_val.insert("four".to_string(), 4);
+    str_to_val.insert("five".to_string(), 5);
+    str_to_val.insert("six".to_string(), 6);
+    str_to_val.insert("seven".to_string(), 7);
+    str_to_val.insert("eight".to_string(), 8);
+    str_to_val.insert("nine".to_string(), 9);
+
+    let first = get_val(line, &str_to_val);
+
+    // now revert all for getting the last
+    let rev_line = revert_str(line);
+    let mut rev_str_to_val: HashMap<String, i32> = HashMap::new();
+    for (k, v) in str_to_val.iter() {
+        let rev_k = revert_str(k.as_str());
+        rev_str_to_val.insert(rev_k, *v);
+    }
+    let last = get_val(rev_line.as_str(), &rev_str_to_val);
+
+    let result = first * 10 + last;
+    result
+}
+
+fn get_val(line: &str, str_to_val: &HashMap<String, i32>) -> i32 {
+    let a = 42;
+    for idx in 0..line.len() {
+        let search = &line[idx..];
+        for (k, v) in str_to_val.iter() {
+            if search.starts_with(k) {
+                return *v;
+            }
+        }
+    }
+    0
+}
+
+fn revert_str(s: &str) -> String {
+    let rev_s = s.chars().rev().collect::<String>();
+    rev_s
 }
