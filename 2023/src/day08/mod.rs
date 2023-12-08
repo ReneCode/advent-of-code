58877bebe6b51681c74f6e9b9ed4f3a587cd8fdd
Reinhard Langner <ReinhardLangner@gmx.de>
2023-12-08 15:22:44 +0100

    day08 move to vec<Node>

--- 2023/src/day08/mod.rs
+++ 2023/src/day08/mod.rs
@@ -11,15 +11,15 @@ enum State {
 
 #[derive(Debug)]
 struct Node {
-    // name: String,
+    name: String,
     left: String,
     right: String,
 }
 
 impl Node {
-    fn new(_name: &str, left: &str, right: &str) -> Self {
+    fn new(name: &str, left: &str, right: &str) -> Self {
         Node {
-            // name: name.to_string(),
+            name: name.to_string(),
             left: left.to_string(),
             right: right.to_string(),
         }
@@ -33,7 +33,7 @@ pub fn day08() {
 
     let mut state = State::ReadInstructions;
     let mut instructions = String::new();
-    let mut nodes: HashMap<String, Node> = HashMap::new();
+    let mut nodes: Vec<Node> = Vec::new();
     for line in lines {
         if line.len() == 0 {
             continue;
@@ -45,11 +45,11 @@ pub fn day08() {
             }
             State::ReadNodes => {
                 let tok = parse::to_str(&line, '=');
-                let name = tok[0].to_string();
+                let name = tok[0];
                 let targets = tok[1].replace('(', "").replace(')', "");
                 let tok = parse::to_str(&targets, ',');
-                let node = Node::new(name.as_str(), tok[0], tok[1]);
-                nodes.insert(name, node);
+                let node = Node::new(name, tok[0], tok[1]);
+                nodes.push(node);
             }
         }
     }
@@ -60,12 +60,12 @@ pub fn day08() {
     println!("Result A: {result_a}");
 }
 
-fn travel(instructions: String, nodes: &HashMap<String, Node>, start: &str, stop: &str) -> usize {
+fn travel(instructions: String, nodes: &Vec<Node>, start: &str, stop: &str) -> usize {
     // println!("{start} / {instruction_idx}");
     let mut idx: usize = 0;
     let mut node_name = start;
     while node_name != stop {
-        let node = nodes.get(node_name).unwrap();
+        let node = nodes.iter().find(|n| n.name == node_name).unwrap();
         let instruction = instructions.chars().nth(idx % instructions.len()).unwrap();
         match instruction {
             'L' => {
