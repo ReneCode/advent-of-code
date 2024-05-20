21bdd73fc3ca726bca16dcebe444f8d5fc9d97be
Reinhard Langner <ReinhardLangner@gmx.de>
2024-05-20 12:08:12 +0200

    day 10

--- 2020/src/day10/mod.rs
+++ 2020/src/day10/mod.rs
@@ -1,13 +1,15 @@
 // day10
 
+use std::{collections::HashMap, vec};
+
 use crate::util::io;
 
 pub fn day10() {
     let lines = io::read_lines("10-example.data").unwrap();
 
-    let numbers: Vec<i32> = lines
+    let numbers: Vec<i64> = lines
         .iter()
-        .map(|line| line.parse::<i32>().unwrap())
+        .map(|line| line.parse::<i64>().unwrap())
         .collect();
 
     let part_1 = calc_steps(&numbers);
@@ -17,7 +19,7 @@ pub fn day10() {
     println!("Part 2: {}", part_2);
 }
 
-fn calc_steps(numbers: &Vec<i32>) -> i32 {
+fn calc_steps(numbers: &Vec<i64>) -> i64 {
     let mut sorted = numbers.clone();
     sorted.sort();
 
@@ -40,12 +42,44 @@ fn calc_steps(numbers: &Vec<i32>) -> i32 {
     diff_1 * diff_3
 }
 
-fn part2(numbers: &Vec<i32>) -> i32 {
+fn part2(numbers: &Vec<i64>) -> i64 {
     let mut sorted = numbers.clone();
     sorted.sort();
 
-    let mut paths = vec![0; sorted.len()];
-    paths[0] = 1;
+    let mut count: u64 = 0;
 
-    0
+    let mut steps: HashMap<i64, i64> = HashMap::new();
+    for nr in sorted.iter().rev() {
+        let possibilities = get_possibilities(&sorted, *nr);
+        if possibilities.len() == 0 {
+            steps.insert(*nr, 1);
+        } else {
+            let mut sum = 0;
+            for p in possibilities {
+                sum += steps.get(&p).unwrap();
+            }
+            steps.insert(*nr, sum);
+        }
+    }
+
+    for nr in sorted.iter() {
+        println!("{}: {}", nr, steps.get(nr).unwrap());
+    }
+    // println!("{:?}", steps);
+
+    let first_nr = sorted[0];
+    let result = steps.get(&first_nr).unwrap();
+
+    *result
+}
+
+fn get_possibilities(numbers: &Vec<i64>, from: i64) -> Vec<i64> {
+    let mut possibilities: Vec<i64> = Vec::new();
+    for i in 1..4 {
+        if numbers.contains(&(from + i)) {
+            possibilities.push(from + i);
+        }
+    }
+
+    possibilities
 }
