92760452e6e96f4729d7730ca5e134b6740d9e8f
Reinhard Langner <ReinhardLangner@gmx.de>
2024-12-02 07:57:43 +0100

    day02/2

--- 2024/src/day02/mod.rs
+++ 2024/src/day02/mod.rs
@@ -14,6 +14,9 @@ pub fn day02() {
 
     let count_safe = levels.iter().filter(|level| is_safe(level)).count();
     println!("Safe levels: {}", count_safe);
+
+    let count_damped_safe = levels.iter().filter(|level| is_damped_safe(level)).count();
+    println!("Damped safe levels: {}", count_damped_safe);
 }
 
 fn is_safe(level: &Vec<i32>) -> bool {
@@ -34,4 +37,24 @@ fn is_safe(level: &Vec<i32>) -> bool {
     }
 
     true
+}
+
+fn is_damped_safe(level: &Vec<i32>) -> bool {
+    if is_safe(level) {
+        return true;
+    }
+
+    for i in 0..level.len() {
+        let one_removed: Vec<i32> = level
+            .iter()
+            .enumerate()
+            .filter(|&(idx, _)| idx != i)
+            .map(|(_idx, nr)| *nr)
+            .collect();
+        if is_safe(&one_removed) {
+            return true;
+        }
+    }
+
+    false
 }
