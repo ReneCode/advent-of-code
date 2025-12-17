b4ffd7a855ca5ca1c050480089de7e96577a37bc
Reinhard Langner <ReinhardLangner@gmx.de>
2025-12-17 19:35:29 +0100

    day7-2

--- 2025/7.py
+++ 2025/7.py
@@ -4,11 +4,8 @@ with open("7.data") as f:
 
 lines = [line.strip("\n") for line in lines]
 
-for line in lines:
-  print(line)
-
-
-
+SPLITTER = "^"
+EMPTY = "."
 
 def find_start(lines):
   top_line = lines[0]
@@ -40,4 +37,48 @@ def part1():
       break
   print("Total count:", total_count)
 
-part1()
+
+def part2():
+  # start from bottom up
+  # collect path counts on a SPLITTER 
+  # as a sum of left and right on a row below 
+
+  height = len(lines)
+  width = len(lines[0])
+  path_counts = [[0 for _ in range(width)] for _ in range(height)]
+  pos = find_start(lines)
+  row = height - 1
+  for w in range(width):
+    path_counts[row][w] = 1
+
+  total = 0
+  for r in range(height-2, -1, -1):
+    if "S" in lines[r]:
+      for c in range(width):
+        val = lines[r][c]
+        if val == "S":
+          total = path_counts[r+1][c]
+          break
+
+    if not SPLITTER in lines[r]:
+      # only ..... in the line
+      for c in range(width):
+        if lines[r+1][c] == SPLITTER:
+          # splitter below, sum counts from left and right
+          path_counts[r][c] = path_counts[r+1][c-1] + path_counts[r+1][c+1]
+        else:
+          path_counts[r][c] = path_counts[r+1][c]
+        
+    else:
+      for c in range(width):
+        val = lines[r][c]
+        if val == EMPTY:
+          path_counts[r][c] = path_counts[r+1][c]
+        elif val == SPLITTER:
+          path_counts[r][c] = 0
+
+
+  print("Total paths:", total)
+
+# part1()
+part2()
